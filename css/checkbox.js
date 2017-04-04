906c67906b47d0f5ecc770fa4430bd705e77b184
Said Muhammad <saidmsaid81@gmail.com>
2017-04-04 17:52:10 +0300

    added dua,hadith

---
+++ css/checkbox.js
@@ -0,0 +1,25 @@
+
+									$(document).ready(function () {
+    $('#checkbox-1ab').change(function () {
+      $('#transliteration').fadeToggle();
+      $('#transliteration1').fadeToggle();
+      $('#transliteration2').fadeToggle();
+      $('#transliteration3').fadeToggle();
+      $('#transliteration4').fadeToggle();
+       $('#transliteration5').fadeToggle();
+       $('#transliteration6').fadeToggle();
+    });
+    $('#checkbox-1ab1').change(function () {
+      $('#translation').fadeToggle();
+      $('#translation1').fadeToggle();
+      $('#translation2').fadeToggle();
+      $('#translation3').fadeToggle();
+      $('#translation4').fadeToggle();
+      $('#translation5').fadeToggle();
+      $('#translation6').fadeToggle();
+      $('#translation7').fadeToggle();
+       $('#translation8').fadeToggle();
+});
+});
+								
+   
