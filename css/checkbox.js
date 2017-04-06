7b37914de9d45bc91b0110ecf1e0e086de23fe63
Said Muhammad <saidmsaid81@gmail.com>
2017-04-07 00:52:47 +0300

    added all content to all tabs

--- css/checkbox.js
+++ css/checkbox.js
@@ -4,21 +4,156 @@
       $('#transliteration').fadeToggle();
       $('#transliteration1').fadeToggle();
       $('#transliteration2').fadeToggle();
-      $('#transliteration3').fadeToggle();
+      $('#transliteration3').fadeToggle(); });
+
+    $('#checkbox-1abpg3').change(function () {
       $('#transliteration4').fadeToggle();
-       $('#transliteration5').fadeToggle();
-       $('#transliteration6').fadeToggle();
+ $('#transliteration5').fadeToggle();});
+
+    $('#checkbox-1abpg2').change(function () {
+      
+       $('#transliteration6').fadeToggle();});
+
+
+
+    $('#checkbox-1abpg4').change(function () {
+       $('#transliteration7').fadeToggle();
+       $('#transliteration8').fadeToggle();
+       $('#transliteration9').fadeToggle();
+       $('#transliteration10').fadeToggle();
+       $('#transliteration11').fadeToggle();
+       $('#transliteration12').fadeToggle();
+       $('#transliteration13').fadeToggle();
+       $('#transliteration14').fadeToggle();
+       $('#transliteration15').fadeToggle();
+       $('#transliteration16').fadeToggle();
+       $('#transliteration17').fadeToggle();
+       $('#transliteration18').fadeToggle();
+       
+       $('#transliteration19').fadeToggle();
+       $('#transliteration20').fadeToggle();
+       $('#transliteration21').fadeToggle();
+       $('#transliteration22').fadeToggle();
+        $('#transliteration23').fadeToggle();
+         $('#transliteration42').fadeToggle();
+$('#transliteration45').fadeToggle();
+$('#transliteration47').fadeToggle();
+ $('#transliteration49').fadeToggle();
+       });
+
+
+     $('#checkbox-1abpg5').change(function () {
+        $('#transliteration24').fadeToggle();
+       $('#transliteration25').fadeToggle();
+       $('#transliteration26').fadeToggle();
+       $('#transliteration27').fadeToggle();
+       $('#transliteration28').fadeToggle();
+       $('#transliteration29').fadeToggle();
+       $('#transliteration30').fadeToggle();
+       $('#transliteration31').fadeToggle();
+       $('#transliteration32').fadeToggle();
+       $('#transliteration33').fadeToggle();
+       $('#transliteration34').fadeToggle();
+       $('#transliteration35').fadeToggle();
+       
+       $('#transliteration36').fadeToggle();
+       $('#transliteration37').fadeToggle();
+       $('#transliteration38').fadeToggle();
+       $('#transliteration39').fadeToggle();
+       $('#transliteration40').fadeToggle();
+       $('#transliteration41').fadeToggle();
+      
+       $('#transliteration43').fadeToggle();
+        $('#transliteration44').fadeToggle();
+         $('#transliteration46').fadeToggle();
+          $('#transliteration48').fadeToggle();
+
+       
+
     });
     $('#checkbox-1ab1').change(function () {
       $('#translation').fadeToggle();
       $('#translation1').fadeToggle();
       $('#translation2').fadeToggle();
       $('#translation3').fadeToggle();
+
+});
+
+       $('#checkbox-1ab1pg3').change(function () {
+     
       $('#translation4').fadeToggle();
       $('#translation5').fadeToggle();
+});
+
+
+       $('#checkbox-1ab1pg2').change(function () {
+      
       $('#translation6').fadeToggle();
+});
+
+
+       $('#checkbox-1ab1pg4').change(function () {
       $('#translation7').fadeToggle();
        $('#translation8').fadeToggle();
+       $('#translation9').fadeToggle();
+       $('#translation10').fadeToggle();
+       $('#translation11').fadeToggle();
+       $('#translation12').fadeToggle();
+       $('#translation13').fadeToggle();
+       $('#translation14').fadeToggle();
+       $('#translation15').fadeToggle();
+       $('#translation16').fadeToggle();
+       $('#translation17').fadeToggle();
+
+       $('#translation18').fadeToggle();
+
+       $('#translation19').fadeToggle();
+
+       $('#translation20').fadeToggle();
+
+       $('#translation21').fadeToggle();
+
+       $('#translation22').fadeToggle();
+        $('#translation23').fadeToggle();
+        $('#translation42').fadeToggle();
+         $('#translation45').fadeToggle();
+         $('#translation47').fadeToggle();
+         $('#translation49').fadeToggle();
+
+});
+
+         $('#checkbox-1ab1pg5').change(function () {
+        $('#translation24').fadeToggle();
+       $('#translation25').fadeToggle();
+       $('#translation26').fadeToggle();
+       $('#translation27').fadeToggle();
+       $('#translation28').fadeToggle();
+       $('#translation29').fadeToggle();
+       $('#translation30').fadeToggle();
+       $('#translation31').fadeToggle();
+       $('#translation32').fadeToggle();
+       $('#translation33').fadeToggle();
+
+       $('#translation34').fadeToggle();
+
+       $('#translation35').fadeToggle();
+
+       $('#translation36').fadeToggle();
+
+       $('#translation37').fadeToggle();
+
+       $('#translation38').fadeToggle();
+        $('#translation39').fadeToggle();
+        $('#translation40').fadeToggle();
+        $('#translation41').fadeToggle();
+        
+        $('#translation43').fadeToggle();
+        $('#translation44').fadeToggle();
+        $('#translation46').fadeToggle();
+        $('#translation48').fadeToggle();
+
+
+
 });
 });
 								
