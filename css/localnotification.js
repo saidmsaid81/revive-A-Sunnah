645506fe67d569325adf80a15d7cd24465efd3a6
Said Muhammad <saidmsaid81@gmail.com>
2017-04-10 04:02:49 +0300

    fixes

--- css/localnotification.js
+++ css/localnotification.js
@@ -14,34 +14,4 @@ document.addEventListener('deviceready', this.onDeviceReady, false);
 		 
 		})	
 	 }
-	 function localnotificationo() {
-	 	cordova.plugins.notification.local.schedule({
-		id: 2,
-		title: "Have you read Surah Al Kahf",
-		text: "Tap here to read now",
-	
-		firstAt: friday_in_morning,
-		every: 10080 // "minute", "hour", "week", "month", "year"
-		
-		})	
-		
-    }
-    function localnotificationoo() {
-	 	cordova.plugins.notification.local.schedule([{
-		id: 3,
-		title: "The Sunnah Revival",
-		text: "Reminder To fast Tomorrow Monday",
-	
-		firstAt: sunday_in_night,
-		every: 10080 // "minute", "hour", "week", "month", "year"
-		,{
-			
-		id: 4,
-		title: "The Sunnah Revival",
-		text: "Reminder To fast Tomorrow Thursday",
-	
-		firstAt: wednesday_in_evening,
-		every: 10080 // "minute", "hour", "week", "month", "year"
-		
-		}]);	
-	 }
+	 
