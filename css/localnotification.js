e5e03502b05c2ec29d41921f4e9c6dc132eb5263
Said Muhammad <saidmsaid81@gmail.com>
2017-04-10 15:14:27 +0300

    fixes

--- css/localnotification.js
+++ css/localnotification.js
@@ -1,6 +1,11 @@
 document.addEventListener('deviceready', this.onDeviceReady, false);
 	 function onDeviceReady() {
-	 	navigator.notification.alert("PhoneGap is working");
+	 	navigator.notification.alert(
+    'Tap <i class="zmdi zmdi-notifications"></i> to manage reminders such as Suratul Kahf reminders on Friday',  // message
+    
+    'Tutorial',            // title
+    'Got It'                  // buttonName
+);
 	 }
 
 	 
