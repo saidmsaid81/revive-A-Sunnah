a7285f80eeb29068558cdda3ac4c332b14f04289
Said Muhammad <saidmsaid81@gmail.com>
2017-04-10 14:39:05 +0300

    fixes

--- css/localnotification.js
+++ css/localnotification.js
@@ -1,17 +1,7 @@
 document.addEventListener('deviceready', this.onDeviceReady, false);
 	 function onDeviceReady() {
-	 	alert('Device ready...')
+	 	navigator.notification.alert("PhoneGap is working");
 	 }
 
-	 function localnotification() {
-	 	cordova.plugins.notification.local.schedule({
-		id: 1,
-		title: "Reminder to recite Surah Al Mulk",
-		text: "Tap here to read now",
-	   
-		firstAt: tomorrow_at_6_am,
-		every: 1440 // "minute", "hour", "week", "month", "year"
-		 
-		})	
-	 }
+	 
 	 
