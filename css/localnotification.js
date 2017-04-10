1b33d3888746548367e59d15baf026f787380863
Said Muhammad <saidmsaid81@gmail.com>
2017-04-11 02:28:43 +0300

    fixes

--- css/localnotification.js
+++ css/localnotification.js
@@ -1,6 +1,12 @@
 document.addEventListener('deviceready', this.onDeviceReady, false);
 	 function onDeviceReady() {
 	 	 welcomeScreen();
+         AppRate.preferences.storeAppURL.android = 'market://details?id=com.ihyausunnah.thesunnahrevival';
+        AppRate.preferences.openStoreInApp = true;
+        AppRate.preferences.displayAppName = 'The Sunnah Revival';
+        AppRate.preferences.usesUntilPrompt = 2;
+         AppRate.preferences.promptAgainForEachNewVersion = false;
+         AppRate.promptForRating();
 	 	
 	 }
 
