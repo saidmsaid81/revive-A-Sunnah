d665fcdb13e76151193a58a66166182e427e48de
Said Muhammad <saidmsaid81@gmail.com>
2017-04-11 00:38:42 +0300

    fixes

--- css/localnotification.js
+++ css/localnotification.js
@@ -7,12 +7,9 @@ function welcomeScreen(){
             var welcome = window.localStorage.getItem("welcome");
             if(!welcome){
                 window.localStorage.setItem("welcome", "1");
-               navigator.notification.alert(
-    'Tap'+ '<i class="zmdi zmdi-notifications"></i>' +'to manage reminders such as Suratul Kahf reminders on Friday',  // message
-    null,
-    'Tutorial',            // title
-    'Got It'                  // buttonName
-); // replace with welcome screen display    
+                $( function() {
+    $( "#dialog" ).dialog();
+  } );    
             }
         }
 
